23f937bb2e011b23b4f260645c68f4816ae6f57c
Shivang <shivang.tripathi@credable.in>
2022-03-04 10:59:14 +0530

    Renamed Class to Selenium Testing

--- src/test/java/org/example/Selenium_Testing.java
+++ src/test/java/org/example/Selenium_Testing.java
@@ -1,16 +1,16 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.junit.Assert.assertEquals;
 
-public class Test {
-    @org.junit.Test
+public class Selenium_Testing {
+    @Test
     public void test_selenium_dot_dev_blog() {
         //WebDriverManager.firefoxdriver().setup();
         WebDriverManager.chromedriver().setup();
