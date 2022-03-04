136a2a8108a8c49278386d125803f7de4840120e
Shivang <shivang.tripathi@credable.in>
2022-03-04 10:57:40 +0530

    Doing Assertion for Text & Title

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,46 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
 
 public class Main {
     public static void main(String[] args) {
 
-        //FirefoxOptions options=new FirefoxOptions();
-        //options.setBinary("/usr/bin/firefox");
-
-        //WebDriver driver=new FirefoxDriver(options);
         WebDriver driver=new ChromeDriver();
-        driver.get("http://google.com");
+        //googletesting(driver);
+        SeleniumTest(driver);
+        //driver.findElement(By.xpath("(a[@href='https://www.guru99.com/introduction-to-selenium.html'])")).click();
+    }
+
+    private static void googletesting(WebDriver driver) {
+        driver.get("https://google.com");
 
         // document.getElementsByName("q")[0]
-        WebElement searchbox=driver.findElement(By.name("q"));
+        WebElement searchbox= driver.findElement(By.name("q"));
         searchbox.click();
 
-       // document.getElementsByName("q")[0].value="Selenium Testing"
+        // document.getElementsByName("q")[0].value="Selenium Testing"
         searchbox.sendKeys("Selenium Testing");
         searchbox.sendKeys(Keys.ENTER);
-        driver.findElement(By.xpath("(a[@href='https://www.guru99.com/introduction-to-selenium.html'])")).click();
-
+    }
+    public static void SeleniumTest(WebDriver driver)
+    {
+        driver.get("https://www.selenium.dev/");
+        driver.findElement(By.linkText("blog"));
+        driver.findElement(By.linkText(""));
+//        driver.findElement(By.cssSelector("#main_navbar > ul > li:nth-child(7) > a")).click();
+//        driver.findElement(By.id("m-blog2016-li")).click();
+//        driver.findElement(By.cssSelector("body > div > div.td-main > div > main > div:nth-child(1) > div > ul > li:nth-child(4) > div > h5 > a")).click();
+        driver.quit();
+    }
+    public static WebDriver ChromeDriver()
+    {
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
     }
 }
