f3793bfb35fe38261ac70e853fe94b57d9eaebbc
Shivang <shivang.tripathi@credable.in>
2022-03-04 11:30:28 +0530

    Used @Before/@BeforeClass/@After Annotations

--- src/test/java/org/example/Selenium_Testing.java
+++ src/test/java/org/example/Selenium_Testing.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,28 +13,41 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.Assert.assertEquals;
 
 public class Selenium_Testing {
+
+    //AAA :Arrange Act & Assert
+    private WebDriver driver;
+    @BeforeClass
+    public static void init()
+    {
+        WebDriverManager.chromedriver().setup();
+    }
+    @Before
+    public void setup()
+    {
+        driver = new ChromeDriver();
+        driver.get("https://selenium.dev");
+        driver.findElement(By.linkText("Blog")).click();
+    }
+    @After
+    public void EndTest()
+    {
+        driver.quit();
+    }
     @Test
     public void test_selenium_dot_dev_blog() {
-        //WebDriverManager.firefoxdriver().setup();
-        WebDriverManager.chromedriver().setup();
 
-        //WebDriver driver = new FirefoxDriver();
-        WebDriver driver = new ChromeDriver();
-
-        driver.get("https://selenium.dev");
-
-        WebElement blog = driver.findElement(By.linkText("Blog"));
-        blog.click();
-
-        WebElement link2016 = driver.findElement(By.id("m-blog2016"));
-        link2016.click();
-
+        driver.findElement(By.id("m-blog2016")).click();
         WebElement element = driver.findElement(By.linkText("Fall Selenium Conf, Save the Date & Call for Speakers!"));
         assertEquals("Fall Selenium Conf, Save the Date & Call for Speakers!", element.getText());
-
         String title = driver.getTitle();
         assertEquals("Blog Posts - 2016 | Selenium", title);
 
-        //driver.quit();
+    }
+    @Test
+    public void test_selenium_dot_dev_blog_2017()
+    {
+        driver.findElement(By.id("m-blog2017")).click();
+        String title = driver.getTitle();
+        assertEquals("Blog Posts - 2017 | Selenium", title);
     }
 }
